dabe4db8885b2d649ded67c543481924401c9531
Mahbubur Rahman <mail@mahbub.me>
2018-12-19 11:36:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ function addElementToBeginningOfArray(array, element) {
   var new_array = [element, ...array];
   return new_array;
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
